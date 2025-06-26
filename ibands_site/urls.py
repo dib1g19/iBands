8f24ea52e365410482e9054f34bf1f1d6baac7ae
Dimitar Bedachev <dib1g19@soton.ac.uk>
2025-06-27 01:38:14 +0300

    Add Google Analytics tracking snippet to base template

--- ibands_site/urls.py
+++ ibands_site/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,19 +22,28 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.shortcuts import render
 
+SHOW_COMING_SOON = os.environ.get("SHOW_COMING_SOON") == "1"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', lambda request: render(request, 'coming_soon.html'), name='coming_soon'),
-    path('', include("store.urls")),
-    path('auth/', include("userauths.urls")),
-    path('customer/', include("customer.urls")),
-    path('blog/', include("blog.urls")),
-    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='userauths/password/password_reset.html'), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='userauths/password/password_reset_done.html'), name='password_reset_done'),
-    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='userauths/password/password_reset_confirmation.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='userauths/password/password_reset_complete.html'), name='password_reset_complete'),
-    path("ckeditor5/", include('django_ckeditor_5.urls')),
 ]
+
+if SHOW_COMING_SOON:
+    urlpatterns += [
+        path('', lambda request: render(request, 'coming_soon.html'), name='coming_soon'),
+    ]
+else:
+    urlpatterns += [
+        path('', include("store.urls")),
+        path('auth/', include("userauths.urls")),
+        path('customer/', include("customer.urls")),
+        path('blog/', include("blog.urls")),
+        path('password-reset/', auth_views.PasswordResetView.as_view(template_name='userauths/password/password_reset.html'), name='password_reset'),
+        path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='userauths/password/password_reset_done.html'), name='password_reset_done'),
+        path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='userauths/password/password_reset_confirmation.html'), name='password_reset_confirm'),
+        path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='userauths/password/password_reset_complete.html'), name='password_reset_complete'),
+        path("ckeditor5/", include('django_ckeditor_5.urls')),
+    ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

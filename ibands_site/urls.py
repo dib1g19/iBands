86ebbd6357fc337ba084cfefc9a4a89dec50c662
Dimitar Bedachev <dib1g19@soton.ac.uk>
2025-07-06 18:26:26 +0300

    Refactor: move robots.txt to templates

--- ibands_site/urls.py
+++ ibands_site/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for ibands_site project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,7 +5,7 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.contrib.sitemaps.views import sitemap
 from store.sitemaps import ProductSitemap, CategorySitemap, StaticViewSitemap
-from django.views.static import serve
+from django.views.generic import TemplateView
 
 sitemaps = {
     "products": ProductSitemap,
@@ -72,12 +55,7 @@ urlpatterns = [
     path("ckeditor5/", include("django_ckeditor_5.urls")),
     path(
         "robots.txt",
-        serve,
-        {
-            "path": "robots.txt",
-            "document_root": settings.STATIC_ROOT,
-            "content_type": "text/plain",
-        },
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
     ),
 ]
 
